c339b720ad17886e5aaac10fe34c459f42a8ea15
Chris Hamons <chrishamons@onesignal.com>
2024-09-12 11:00:50 -0500

    dev: Move to async fetch for get

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{future::Future, path::Path};
 
 use eyre::Result;
 use rusqlite::{params, Connection};
@@ -19,10 +19,11 @@ impl ResponseCache {
         Ok(ResponseCache { connection })
     }
 
-    pub fn get<F>(&self, key: impl AsRef<str>, mut fetch: F) -> Result<Vec<u8>>
-    where
-        F: FnMut() -> Result<Vec<u8>>,
-    {
+    pub async fn get(
+        &self,
+        key: impl AsRef<str>,
+        fetch: impl Future<Output = eyre::Result<Vec<u8>>>,
+    ) -> Result<Vec<u8>> {
         let mut fetch_statement = self
             .connection
             .prepare("SELECT value FROM cached WHERE key = (?1)")?;
@@ -33,7 +34,7 @@ impl ResponseCache {
         match cache_row {
             Some(cache_row) => Ok(cache_row.get(0)?),
             None => {
-                let value = fetch()?;
+                let value = fetch.await?;
 
                 self.connection.execute(
                     "INSERT INTO cached VALUES((?1), (?2))",
@@ -65,72 +66,87 @@ mod test {
         }
     }
 
-    #[test]
-    fn round_trip_multiple() {
+    #[tokio::test]
+    async fn round_trip_multiple() {
         let test = TestHarness::new();
 
         let value_one = "1".as_bytes().to_vec();
         let value_two = "2".as_bytes().to_vec();
 
         assert_eq!(
-            test.cache.get("1", || Ok(value_one.clone())).unwrap(),
+            test.cache
+                .get("1", async { Ok(value_one.clone()) })
+                .await
+                .unwrap(),
             value_one
         );
         assert_eq!(
-            test.cache.get("2", || Ok(value_two.clone())).unwrap(),
+            test.cache
+                .get("2", async { Ok(value_two.clone()) })
+                .await
+                .unwrap(),
             value_two
         );
         assert_eq!(
-            test.cache.get("1", || Ok(value_one.clone())).unwrap(),
+            test.cache
+                .get("1", async { Ok(value_one.clone()) })
+                .await
+                .unwrap(),
             value_one
         );
     }
 
-    #[test]
-    fn fetch_count() {
+    #[tokio::test]
+    async fn fetch_count() {
         let test = TestHarness::new();
 
         let mut fetched_count = 0;
-        let mut fetch = || {
-            fetched_count += 1;
-            Ok("1".as_bytes().to_vec())
-        };
 
-        let result1 = test.cache.get("1", &mut fetch).unwrap();
-        let result2 = test.cache.get("1", &mut fetch).unwrap();
-        let result3 = test.cache.get("1", &mut fetch).unwrap();
+        for _ in 0..3 {
+            let result = test
+                .cache
+                .get("1", async {
+                    fetched_count += 1;
+                    Ok("1".as_bytes().to_vec())
+                })
+                .await
+                .unwrap();
 
-        assert_eq!(result1, "1".as_bytes().to_vec());
-        assert_eq!(result2, "1".as_bytes().to_vec());
-        assert_eq!(result3, "1".as_bytes().to_vec());
+            assert_eq!(result, "1".as_bytes().to_vec());
+        }
+
         assert_eq!(1, fetched_count);
     }
 
-    #[test]
-    fn sustains_data() {
+    #[tokio::test]
+    async fn sustains_data() {
         let test = TestHarness::new();
 
         assert_eq!(
-            test.cache.get("1", || Ok("1".as_bytes().to_vec())).unwrap(),
+            test.cache
+                .get("1", async { Ok("1".as_bytes().to_vec()) })
+                .await
+                .unwrap(),
             "1".as_bytes().to_vec()
         );
 
         let cache = ResponseCache::new(test.file.path()).unwrap();
         assert_eq!(
-            cache.get("1", || panic!()).unwrap(),
+            cache.get("1", async { panic!() }).await.unwrap(),
             "1".as_bytes().to_vec()
         );
     }
 
-    #[test]
-    fn stores_uuid() {
+    #[tokio::test]
+    async fn stores_uuid() {
         let test = TestHarness::new();
 
         let data = uuid::Uuid::new_v4();
 
         let fetched_data = test
             .cache
-            .get("1", || Ok(data.as_bytes().to_vec()))
+            .get("1", async { Ok(data.as_bytes().to_vec()) })
+            .await
             .unwrap();
 
         assert_eq!(uuid::Uuid::from_slice(&fetched_data).unwrap(), data);
